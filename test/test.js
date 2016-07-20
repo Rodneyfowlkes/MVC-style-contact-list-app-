762b9a886fdf45f4f8dcef306d57fcc24ff54ec5
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-07-20 10:00:48 -0400

    More styling but it still looks ugly

--- test/test.js
+++ test/test.js
@@ -2,19 +2,21 @@
 import chai from 'chai';
 
 // Import Any Files to Test
+import {Contact} from '../src/js/models/contact.js';
+import {AppController} from '../src/js/controller/app-controller.js';
 
 // Set Chai Constants
 const expect = chai.expect;
 const should = chai.should();
 const assert = chai.assert;
 
-describe('Something We Want To Test', function () {
+describe('The app controlller', function () {
 
-  describe('Testing the Creation of Something', function () {
+  describe('There should be an instance of AppController', function () {
 
-    it('should exist after we create it', function () {
-      let x = new Object;
-      expect(x).to.be.an.instanceof(Object);
+    it('should be an instance of AppController', function () {
+      let x = new AppController;
+      expect(x).to.be.an.instanceof(AppController);
     });
 
   });
